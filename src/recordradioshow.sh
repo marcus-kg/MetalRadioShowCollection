c4f922062c6519947f817ed29f4b1008cf896a55
Marcus Küppers-Grün <marcus.kueppers@web.de>
2012-04-24 12:11:58 +0200

    begining of script to record

---
+++ src/recordradioshow.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# script to record a radio show
+# params:
+# $1 radioRecordingRootDir
+# $2 NameOfStation
+# $3 NameOfShow
+# $4 duration in s
+# $5 p | s  #  filePertrack |  SingleFile
+# $6 ProgramToExecuteAfterRecording # optional
